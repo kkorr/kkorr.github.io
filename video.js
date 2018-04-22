fb36468d25218f007a41579fad78e37bc82718a0
Konstantin Kornilov <kkorr@users.noreply.github.com>
2018-04-23 02:23:46 +0300

    Update video.js

--- video.js
+++ video.js
@@ -1,21 +1,41 @@
-		if($(".bg-container-youtube").is(':visible')){
-    if($(window).width() >= 1200){
-        $('.player').mb_YTPlayer();
-    }
-    else{
-        $(".bg-container-youtube").backstretch([
-            "img/slideshow/sample.jpg",		//-- CHANGE WITH YOUR IMAGE URL
-            "img/slideshow/sample2.jpg"
-            ],{
-            duration:6000,
-            fade:'normal'
-        });
-    }
-}
-
- $('#play-video').on('click', function(ev) {
- 
-    $("#video")[0].src += "&autoplay=1";
-    ev.preventDefault();
- 
+		if($(".bg-container-youtube").is(':visible')){
+
+    if($(window).width() >= 1200){
+
+        $('.player').mb_YTPlayer();
+
+    }
+
+    else{
+
+        $(".bg-container-youtube").backstretch([
+
+            "asad-atoll-biology-169191.jpg",		//-- CHANGE WITH YOUR IMAGE URL
+
+            "beautiful-blur-bracelet-313702.jpg"
+
+            ],{
+
+            duration:6000,
+
+            fade:'normal'
+
+        });
+
+    }
+
+}
+
+
+
+ $('#play-video').on('click', function(ev) {
+
+ 
+
+    $("#video")[0].src += "&autoplay=1";
+
+    ev.preventDefault();
+
+ 
+
   });
